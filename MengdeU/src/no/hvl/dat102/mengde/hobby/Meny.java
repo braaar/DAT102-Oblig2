1d94d064632a97bb3df2dcc5c8e2cbafc7e09db7
ShnuguhutheFifth <jorgen.gjelsten@gmail.com>
2019-03-12 13:26:40 +0100

    Ø til oe

--- MengdeU/src/no/hvl/dat102/mengde/hobby/Meny.java
+++ MengdeU/src/no/hvl/dat102/mengde/hobby/Meny.java
@@ -11,12 +11,12 @@ public class Meny {
 	}
 	public void start() {
 		System.out.println("Dette er et valg-meny program for å lagre personer med hobbyer og se om de har de samme hobbyene");
-		boolean kjører = true;
+		boolean kjoerer = true;
 		Medlem ny;
 		int valg;
 		String medlemsnavn;
 		Scanner info = new Scanner(System.in);
-		while (kjører == true) {
+		while (kjoerer == true) {
 			System.out.println("Velg operasjonen du vil gjennomføre:");
 			System.out.println("1. Legg til nytt medlem\n2. Skriv hobbyliste til et medlem, gitt navn\n3. Sjekke etter partner"
 					+ ", gitt navn\n4. Tilbaksestilling av statusindeks på et medlem\n5. Skriv ut par med info angående hobby\n6. Slutt");
@@ -50,7 +50,7 @@ public class Meny {
 					tekstgr.skrivParListe(arkiv);
 					valg = Integer.parseInt(info.nextLine());
 				case 6:
-					kjører = false;
+					kjoerer = false;
 					System.out.print("Ha en fin dag videre");
 			}
 		
