dfadbfcee5d83f9a600742a02f75b9d53702f0bd
Brage Sekse Aarset <brage.aarset@gmail.com>
2019-03-13 19:22:58 +0100

    filer til oppg 5 og 6

---
+++ SorteringOgSoekingFyllUt/src/no/hvl/dat102/SoekingOgSortering.java
@@ -0,0 +1,162 @@
+package no.hvl.dat102;
+
+public class SoekingOgSortering {
+
+	/*************************************************************************************************/
+	// Søkealgoritmer
+	/*************************************************************************************************/
+
+	/**
+	 * Metoden gjør et lineært søk i en tabell av usorterte data
+	 * 
+	 * @param data
+	 *            er data som skal søkes i
+	 * @param min
+	 *            er startindeks
+	 * @param maks
+	 *            er sluttindeks
+	 * @param el
+	 *            er verdien vi søker etter
+	 * @return sann om elementet ble funnet ellers usann
+	 */
+	public static <T extends Comparable<T>> boolean linearSoekU(T[] data, int min, int maks, T el) {
+		int indeks = min;
+		boolean funnet = false;
+
+		while (indeks <= maks && !funnet) {
+			if (data[indeks].compareTo(el) == 0)
+				funnet = true;
+			indeks++;
+		} // while
+		return funnet;
+	}// metode
+
+	/**
+	 * Metoden gjør et lineært søk i en sortert tabell av data
+	 * 
+	 * @param data
+	 *            er data som skal søkes i
+	 * @param min
+	 *            er startindeks
+	 * @param maks
+	 *            er sluttindeks
+	 * @param el
+	 *            verdien vi søker etter
+	 * @return sann hvis funnet ellers usann
+	 */
+	public static <T extends Comparable<T>> boolean linearSoekS(T[] data, int min, int maks, T el) {
+		int indeks = min;
+		boolean funnet = false;
+
+		while (indeks < maks && data[indeks].compareTo(el) < 0) {
+			indeks++;
+		} // while
+
+		if (data[indeks].compareTo(el) == 0) {
+			funnet = true;
+		}
+
+		return funnet;
+
+	}// metode
+
+	/**
+	 * Metoden gjør et binært søk i en *sortert* tabell av data
+	 * 
+	 * @param data
+	 *            er data som skal søkes i
+	 * @param min
+	 *            er startindeks
+	 * @param maks
+	 *            er sluttindeks
+	 * @param el
+	 *            er elementet vi søker etter
+	 * @return sann om elementet ble funnet ellers usann.
+	 */
+
+	
+
+	// Alt 1
+	public static <T extends Comparable<T>> boolean binaerSoek1(T[] data, int min, int maks, T el) {
+		boolean funnet;
+		int midtpunkt = (min + maks) / 2;
+		int resultat = el.compareTo(data[midtpunkt]);
+
+		if (min > maks) { // basistilfelle, ingen element
+			funnet = false;
+		} else if (resultat == 0) {
+			funnet = true;
+		} else if (resultat < 0) {
+			funnet = binaerSoek1(data, min, midtpunkt - 1, el);
+		} else {
+			funnet = binaerSoek1(data, midtpunkt + 1, maks, el);
+		}
+
+		return funnet;
+
+	}
+
+	/* Rekursiv binærsøk som returnerer indeks */
+	// Alt 2
+	public static <T extends Comparable<T>> int binaerSoek2(T[] data, int min, int maks, T el) {
+		// Returnerer indeksen til målelementet hvis det fins ellers -1
+		if (min > maks) { // basistilfelle, ingen element
+			return -1;
+		}
+
+		int midtpunkt = (min + maks) / 2;
+		int resultat = el.compareTo(data[midtpunkt]);
+		if (resultat == 0) { // basistilfelle, finner elementet
+			return midtpunkt;
+		}
+		if (resultat < 0) {// ( vil her også fungere med else if)
+			return binaerSoek2(data, min, midtpunkt - 1, el);
+		} else { // resultat > 0
+			return binaerSoek2(data, midtpunkt + 1, maks, el);
+		}
+	}// metode
+
+	/* Ikke rekursiv binærsøk som returnerer indeks */
+	// Alt 3 -fyll ut
+	public static <T extends Comparable<T>> int binaerSoek3(T[] data, int min, int maks, T el) {
+		return maks;
+		// Returnerer indeksen til målelementet hvis det fins ellers -1
+		// Fyll ut og kjør metoden
+	}//
+
+	/*************************************************************************************************/
+	// Sorteringsalgoritmer
+	/*************************************************************************************************/
+
+	/**
+	 * Utvalgssortering
+	 * 
+	 * @param data
+	 *            er data som skal sorteres
+	 */
+	public static <T extends Comparable<T>> void utvalgssortering(T[] data) {
+		int minste;
+		T temp;
+		for (int neste = 0; neste < data.length - 1; neste++) {
+			minste = neste;
+			for (int sok = neste + 1; sok < data.length; sok++) {
+				if (data[sok].compareTo(data[minste]) < 0) {
+					minste = sok;
+				}
+			} // indre for-løkke
+
+			/** Bytt verdiene */
+			temp = data[minste];
+			data[minste] = data[neste];
+			data[neste] = temp;
+		} // ytre for-løkke
+	}// metode
+
+	/**
+	 * Sortering ved innsetting
+	 * 
+	 * @param data
+	 *            er data som skal sorteres
+	 */
+	// ...
+}
